4cd6ee043418b60bf3f62848b940dc72929ae501
nadar93 <ahmadnadar@gmail.com>
2018-03-26 17:01:58 +0200

    S

--- _Project/LMS/login/admin.py
+++ _Project/LMS/login/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import LMSUser
+from .models import Course
+from .models import Sheet
+from .models import Exercise
+from .models import Question
+
+admin.site.register(LMSUser)
+
+admin.site.register(Course)
+
+admin.site.register(Sheet)
+
+admin.site.register(Exercise)
+
+admin.site.register(Question)
