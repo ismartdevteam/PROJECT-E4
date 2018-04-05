0e7e19ae69e33ebbfdd5a4068615fb79040e4a56
Ulziiburen Dojrpurev <ulziiburend@gmail.com>
2018-04-05 19:10:38 +0200

    teachwer url

--- _Project/LMS/urls.py
+++ _Project/LMS/urls.py
@@ -9,10 +9,7 @@ path('admin/', admin.site.urls),
 path('login/', include('login.urls')),
 path('anticheat/', include('anticheat.urls')),
 path('student/', include('student.urls')),
-<<<<<<< HEAD
+
 url('teacher/', include('teacher.urls'))
 
-=======
-path('teacher/', include('teacher.urls'))
->>>>>>> 34efe62df7c510b73686f706054812b1aff0f98e
 ]
