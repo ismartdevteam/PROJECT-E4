c2d0eaf42efc1f5d51ed56ed537ba057d12c018f
Ulziiburen Dojrpurev <ulziiburend@gmail.com>
2018-04-11 19:06:56 +0200

    teacher exercises

--- _Project/LMS/teacher/urls.py
+++ _Project/LMS/teacher/urls.py
@@ -7,6 +7,8 @@ url(r'^$', views.index ),
 url('courses_view', views.courses_view),
 url(r'^course/(\d+)/$', views.course_detail),
 url('exercises_view', views.exercises_view),
+url(r'^exercise/(\d+)/$', views.exercise_detail),
+# url('students_view', views.students_view),
 url('sheets_view', views.sheets_view),
 url(r'^activities', views.activities,name='activities'),
 ]
